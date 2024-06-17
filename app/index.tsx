755fa70b7f9197fab21842a66ce8dcdaf82dd623
shiva00007 <shivas91919@gmail.com>
2024-06-17 12:12:35 +0530

    Setup and FileStruture Done

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center bg-blue-600">
+      <Text className="font-pextralight">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
