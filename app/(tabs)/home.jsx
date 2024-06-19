af9124a7cb231d83139b391b091b16430a75a583
shiva00007 <shivas91919@gmail.com>
2024-06-19 12:51:07 +0530

    home page empty screen done

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,67 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, FlatList, Image } from "react-native";
+import React, { useState } from "react";
+import { images } from "@/constants";
+import SearchInput from "@/components/SearchInput";
+import Trending from "@/components/Trending";
+import EmptyState from "@/components/EmptyState";
+import { RefreshControl } from "react-native-gesture-handler";
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    //when new video contain
+
+    setRefreshing(false);
+  };
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={[{ id: 1, id: 2, id: 3 }]}
+        keyExtractor={(item) => item.id}
+        renderItem={(item) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-semibold text-white">Shiva</Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="h-9 w-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+
+            <SearchInput />
+
+            <View className="w-full flex-1 pt-5 mb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Videos
+              </Text>
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No vidoes Found"
+            subtitle="Be the First one too upload a video"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
   );
 };
 
---
+++ components/SearchInput.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+
+import { icons, images } from "../constants";
+
+const SearchInput = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center space-x-4">
+      <TextInput
+        className="flex-1 mt-0.5 text-white font-pregular"
+        value={value}
+        placeholder="Search For a Video Topic"
+        placeholderTextColor="#7B7B8B"
+        onChangeText={handleChangeText}
+        secureTextEntry={title === "Password" && !showPassword}
+        {...props}
+      />
+      <TouchableOpacity>
+        <Image source={icons.search} className="h-5 w-5" resizeMode="contain" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput;
